32a02500be2b478e60a39b278061efaf5f309887
MartinezElizabeth <silvcher29@gmail.com>
2024-10-20 14:47:43 -0300

    Agregando listado de archivos en README.md

---
+++ Tema2.php
@@ -0,0 +1,36 @@
+<?php
+//Condición 1
+$valor = rand (1,10);
+echo "El valor sorteado es $valor".PHP_EOL;
+if ($valor<= 5 ){
+    echo " Es menor o igual  a 5";
+}else{
+    echo "Es mayor a 5 ";
+
+
+}
+//Condición 2 
+echo PHP_EOL;
+$valor = rand(1,100);
+echo "El valor sorteado es $valor".PHP_EOL;
+if ($valor <=9){
+        echo"Tiene un digito ";
+}else{
+    if($valor <= 100){
+        echo"Tiene 2 digitos";
+
+    }else{
+        echo "Tiene 3 digitos ";
+    }
+}
+//Condición 3 
+echo PHP_EOL;
+$valor = rand(1,100);
+echo "El valor sorteado es $valor".PHP_EOL;
+if ($valor <= 9 ){
+    echo"Tiene un digito ";
+}elseif ($valor <100){
+    echo"Tiene 2 digitos ";
+}else{
+    echo "Tiene 3 digitos";
+}
